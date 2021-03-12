e93a58482c46b8a38c5decfe2d3317d3ee823834
Rybak-a87 <rybak.a87@gmail.com>
2021-03-12 21:13:07 +0200

    ver.0.0.3

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#$whnc-l2vf6yuz4#(-49q$k+e%7ksn@n%0#2e#o9k5^l1z30k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
